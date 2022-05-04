39fff5a967f3e2db1d730700dbb5fa15deb949c8
aramilevzakhar <ar.z.v@yandex.ru>
2022-05-04 22:27:41 +0500

    up

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 process.env.NTBA_FIX_319 = 1;
 
 const TelegramBot = require('node-telegram-bot-api');
-const axios = require('axios');
+// const axios = require('axios');
 const _ = require('lodash');
-const request = require('requests');
+// const request = require('request');
 const Math = require('math');
-
+const cheerio = require('cheerio');
+const ndl = require('needle');
+const wiki = require('wikipedia');
 //const config = require('config');
 
 const pg = require('pg');
 const fs = require('fs');
+const { val } = require('cheerio/lib/api/attributes');
+const { regexpToText } = require('nodemon/lib/utils');
+const { summary } = require('wikipedia');
 
 let token = fs.readFileSync('token.json', 'utf-8');
 token = JSON.parse(token);
@@ -45,34 +50,86 @@ bot.onText(/\/getid/, async (msg) => {
 });
 
 bot.onText(/\/getCourse (.+)/, async (msg, [source, match] ) => {
-	// console.log(msg);
-	// console.log(source);
-	// console.log(match);
-	const { chat: { id }} = msg
+	const { chat: { id }} = msg;
 	await bot.sendMessage(id, match)
 });
 
-bot.onText(/\/wiki (.+)/, (msg, math) => {
-  axios
-  .get('https://en.wikipedia.org/wiki/Quantum_computing')
-  .then(res => {
-    
-    
-    console.log(`statusCode: ${res.status}`);
-    // console.log(res);
-  })
-  .catch(error => {
-    console.error(error);
+function sum(msg, match){ 
+  return new Promise(function(resolve, reject){
+      const result = x + y;
+      resolve(result);
   });
+}
+
+bot.onText(/\/wiki (.+)/, (msg, match) => {
+  const value = async (msg, match) => {
+    let set = match[1].split(' ');
+    
+    
+    let query = set[0];
+    let language = set[1] ?? 'ru';
+
+    const newUrl = await wiki.setLang(language);
+    let page = await wiki.page(query);
+    let summary = await page.summary();
+
+
+    return Promise.resolve(summary.extract);
+  };
+  const res = value(msg, match)
+  .then(async (p1) => {
+    await bot.sendMessage(msg.chat.id, p1);
+  })
+  .catch(async (e) => { 
+    await bot.sendMessage(msg.chat.id, "Not found");
+    console.log("not found")
+  });
+
+
 });
+
+//   console.log(query);
+//   // bot.sendMessage(msg.chat.id, );
+//   try {
+//     page = await wiki.page(query);
+//     console.log(page);
+//     summary = await page.summary();
+//     // await bot.sendMessage(msg.chat.id, summary);
+//   } catch (error) {
+//     console.log(error);
+//   }
+//   return page;
+//   await bot.sendMessage(msg.chat.id, page);
+//   // console.log(`this is ${summary}`);
+// }).;
 
 bot.onText(/\/help/, async (msg, error) => {
   const chatId = msg.chat.id;
 
-	await bot.sendMessage(chatId, "/help - Справка\n" +
+	await bot.sendMessage(chatId, 
+    "/help - Справка\n" +
     "/getSource - Вывести текущий курс валют\n" +
-    "/getSource [...] - Вывести указанные курсы валют\n");
+    "/getSource [...] - Вывести указанные курсы валют\n" + 
+    "/random - Вывести случайное число\n" +
+    "/setNick [nickname]\n" + 
+    "/wiki [request] [language] по умолчанию русский(ru)\n"
+    
+    
+  );
 });
+
+bot.onText(/\/random/, async (msg, info) => {
+  const chatId = msg.chat.id;
+  if (msg.hasOwnProperty('reply_to_message')) {
+    console.log(msg.text);  
+    
+    let number = getRandomArbitrary(0,1000);
+    await bot.sendMessage(chatId, number);
+  }
+});
+
+
+
 
 bot.on('message', async (msg, info) => {
   const chatId = msg.chat.id;
@@ -83,55 +140,6 @@ bot.on('message', async (msg, info) => {
 		}
 	} else if (msg.hasOwnProperty('reply_to_message')) {
     console.log(msg.text);  
-    
-    // let number = getRandomArbitrary(0,1000);
-    // await bot.sendMessage(chatId, number);
-
-    bot.sendMessage(msg.chat.id, "Select channel", {
-      "reply_markup": {
-        "inline_keyboard": [
-          [
-            {
-              text: "Channel 1",
-              callback_data: "click",
-            },
-          ],
-          [
-            {
-              text: "Channel 2",
-              callback_data: "click2",
-            },
-          ],
-          [
-            {
-              text: "Channel 3",
-              callback_data: "click",
-            },
-          ],
-          [
-            {
-              text: "Channel 4",
-              callback_data: "click2",
-            },
-          ],
-          [
-            {
-              text: "Send post to channel",
-              callback_data: "click2",
-            },
-          ],
-        ],
-        
-      },
-      
-    });
-
-
   }
-
-
-
 });
-
-
 
--- index.js
+++ index.js
@@ -1,41 +1,91 @@
-// const request = require("request");
-// url = 'https://ru.wikipedia.org/wiki/%D0%9A%D0%B2%D0%B0%D0%BD%D1%82%D0%BE%D0%B2%D1%8B%D0%B9_%D0%BA%D0%BE%D0%BC%D0%BF%D1%8C%D1%8E%D1%82%D0%B5%D1%80';
+//const request = require("request");
+//url = 'https://ru.wikipedia.org/wiki/%D0%9A%D0%B2%D0%B0%D0%BD%D1%82%D0%BE%D0%B2%D1%8B%D0%B9_%D0%BA%D0%BE%D0%BC%D0%BF%D1%8C%D1%8E%D1%82%D0%B5%D1%80';
 //query = "Sex"
-//url = `https://en.wikipedia.org/w/api.php?action=opensearch&search="+ ${query} +"&format=json`;
-//url = `'https://en.wikipedia.org/wiki/Sexual_intercourse`
-// request(url, function (err, response, body) {
-//   if (err) {
-//     var error = "cannot connect to the server";
-//     console.log(error);
-//   } else {
-//     let wiki = JSON.parse(body);
-//     console.log(wiki);
-//   }
-// });
+//url = `https://en.wikipedia.org/w/api.php?action=opensearch&search="+ ${query} +"&format=xml`;
+//
+//
+//
+//request(url, function (err, response, body) {
+//  if (err) {
+//    var error = "cannot connect to the server";
+//    console.log(error);
+//  } else {
+//    console.log(body)
+//    let wiki = JSON.parse(body);
+//    console.log(wiki);
+//  }
+//});
+//const wiki = require('wikipedia'); 
+//const { wikiSummary, summaryError } = require('wikipedia'); 
+//const { summary } = require('wikipedia' );
+//
+//(async () => {
+//	try {
+//        let summary: wikiSummary; //sets the object as type wikiSummary
+//		summary = await wiki.summary('Batman');
+//		console.log(summary);
+//        let summary2 = await summary('Batman');//using summary directly
+//	} catch (error) {
+//		console.log(error);
+//		//=> Typeof summaryError, helpful in case you want to handle this error separately
+//	}
+//})();
 
-const axios = require('axios');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+
+
+
+//
+//const wiki = require('wikipedia');
+//
+//(async () => {
+//	try {
+//		const page = await wiki.page('Batman');
+//		console.log(page);
+//		//Response of type @Page object
+//		const summary = await page.summary();
+//		console.log(summary);
+//		//Response of type @wikiSummary - contains the intro and the main image
+//	} catch (error) {
+//		console.log(error);
+//		//=> Typeof wikiError
+//	}
+//})();
+//
+//
+//
+//
+
+
+
+
+
+
+
+
+
+// const axios = require('axios');
+// const jsdom = require("jsdom");
+// const { JSDOM } = jsdom;
 
 // var regex = /<p>\w+<p>/g;
-let regex = /html(?=[^>]*<html)/
-// names.split(pattern);
+// let regex = /html(?=[^>]*<html)/
+// // names.split(pattern);
 
-axios.get('https://en.wikipedia.org/wiki/Quantum_computing').then(res => {
-  // console.log(`statusCode: ${res.status}`);
-  // console.log(res.status);
-  res = res.data;
-  // res = res.getElementById('toc');
+// axios.get('https://en.wikipedia.org/wiki/Quantum_computing').then(res => {
+//   // console.log(`statusCode: ${res.status}`);
+//   // console.log(res.status);
+//   res = res.data;
+//   // res = res.getElementById('toc');
 
-  const dom = new JSDOM(res);
-  console.log();
-  // console.log(res);
-  // res = res.search(regex);
-  // console.log(res);
-  // console.log(res.search(regex));
-}).catch((error) => {
-  console.error(error);
-});
+//   const dom = new JSDOM(res);
+//   console.log();
+//   // console.log(res);
+//   // res = res.search(regex);
+//   // console.log(res);
+//   // console.log(res.search(regex));
+// }).catch((error) => {
+//   console.error(error);
+// });
 
 
 // var request = require(‘request’);
